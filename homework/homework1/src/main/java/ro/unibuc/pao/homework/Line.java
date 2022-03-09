52c11813cf385612bdc457a96e22afcd5d30038f
Mihai Micuda <mihai.micuda@endava.com>
2022-03-09 17:57:10 +0200

    homework 1

---
+++ homework/homework1/src/main/java/ro/unibuc/pao/homework/Line.java
@@ -0,0 +1,19 @@
+package ro.unibuc.pao.homework;
+
+public class Line {
+    private Point p1;
+    private Point p2;
+
+    public Line() {
+
+    }
+
+    public Line(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    //TODO: add code here
+
+
+}
---
+++ homework/homework1/src/main/java/ro/unibuc/pao/homework/ZigZagLine.java
@@ -0,0 +1,26 @@
+package ro.unibuc.pao.homework;
+
+import java.util.List;
+
+public class ZigZagLine extends Line {
+
+    //TODO: add code here
+
+    public ZigZagLine(List<Point> points) {
+        //TODO: add code here
+    }
+
+    public ZigZagLine(Point... points) {
+        //TODO: add code here
+    }
+
+    public void addPoint(Point point) {
+        //TODO: add code here
+    }
+
+    public int getLength() {
+        //TODO: add code here
+        return 0;
+    }
+
+}
