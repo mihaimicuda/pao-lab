d103318be75eb9e42cb4b40d1753d569e08ac281
Statescu Relu <statescu.relu@gmail.com>
2022-03-11 01:04:02 +0200

    Tema 1

--- homework/homework1/src/main/java/ro/unibuc/pao/homework/Line.java
+++ homework/homework1/src/main/java/ro/unibuc/pao/homework/Line.java
@@ -15,5 +15,23 @@ public class Line {
 
     //TODO: add code here
 
+    public int getLengthOfLine() {
+        return (int) Math.sqrt(Math.pow((p2.getX() - p1.getX()), 2) + Math.pow((p2.getY() - p1.getY()), 2));
+    }
 
+    public void setP1(Point p1) {
+        this.p1 = p1;
+    }
+
+    public void setP2(Point p2) {
+        this.p2 = p2;
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
 }
