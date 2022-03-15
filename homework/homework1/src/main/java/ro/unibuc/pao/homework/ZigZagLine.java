3744bca8ec2ed0081512ec31d172608a8964f37d
Rares3110 <68992409+Rares3110@users.noreply.github.com>
2022-03-15 17:00:01 +0200

    Update ZigZagLine.java

--- homework/homework1/src/main/java/ro/unibuc/pao/homework/ZigZagLine.java
+++ homework/homework1/src/main/java/ro/unibuc/pao/homework/ZigZagLine.java
@@ -1,26 +1,32 @@
 package ro.unibuc.pao.homework;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class ZigZagLine extends Line {
 
-    //TODO: add code here
+    private List<Point> lista;
 
     public ZigZagLine(List<Point> points) {
-        //TODO: add code here
+        lista = new ArrayList<Point>(points);
     }
 
     public ZigZagLine(Point... points) {
-        //TODO: add code here
+        lista = new ArrayList<Point>();
+        for(Point i : points)
+            lista.add(i);
     }
 
     public void addPoint(Point point) {
-        //TODO: add code here
+        lista.add(point);
     }
 
     public int getLength() {
-        //TODO: add code here
-        return 0;
+        int len = 0;
+        for(int i = 0; i < lista.size() - 1; i++)
+            len += Math.sqrt(
+                Math.pow(lista.get(i).getX() - lista.get(i + 1).getX(), 2) +
+                Math.pow(lista.get(i).getY() - lista.get(i + 1).getY(), 2)
+            );
+        return len;
     }
-
 }
