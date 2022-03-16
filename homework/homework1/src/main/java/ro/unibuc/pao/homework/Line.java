96da46b99ef64549f1fa77b068eb4ef998ec0dac
Cezar Trancanau <82811776+cezartrancanau@users.noreply.github.com>
2022-03-16 18:20:38 +0200

    homework 1 done

--- homework/homework1/src/main/java/ro/unibuc/pao/homework/Line.java
+++ homework/homework1/src/main/java/ro/unibuc/pao/homework/Line.java
@@ -5,15 +5,10 @@ public class Line {
     private Point p2;
 
     public Line() {
-
     }
 
     public Line(Point p1, Point p2) {
         this.p1 = p1;
         this.p2 = p2;
     }
-
-    //TODO: add code here
-
-
 }
--- homework/homework1/src/main/java/ro/unibuc/pao/homework/ZigZagLine.java
+++ homework/homework1/src/main/java/ro/unibuc/pao/homework/ZigZagLine.java
@@ -1,26 +1,33 @@
 package ro.unibuc.pao.homework;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class ZigZagLine extends Line {
 
-    //TODO: add code here
+    private List<Point> points;
 
     public ZigZagLine(List<Point> points) {
-        //TODO: add code here
+        this.points.addAll(points);
     }
 
     public ZigZagLine(Point... points) {
-        //TODO: add code here
+        this.points = new ArrayList<Point>();
+        for(int i = 0; i < points.length; i++) {
+            this.points.add(points[i]);
+        }
     }
 
     public void addPoint(Point point) {
-        //TODO: add code here
+        points.add(point);
     }
 
     public int getLength() {
-        //TODO: add code here
-        return 0;
+        int sum = 0;
+        for(int i = 1; i < this.points.size(); i++){
+            int d = (int) Math.sqrt(Math.pow(this.points.get(i).getX() - this.points.get(i-1).getX(), 2) + Math.pow(this.points.get(i).getY() - this.points.get(i-1).getY(), 2));
+            sum += d;
+        }
+        return sum;
     }
-
 }
