d29565b98cdefd7b565390a3094694e91e0d7ae0
MatLasc <80466069+MatLasc@users.noreply.github.com>
2022-03-16 16:02:52 +0200

    Update Point.java

--- homework/homework1/src/main/java/ro/unibuc/pao/homework/Point.java
+++ homework/homework1/src/main/java/ro/unibuc/pao/homework/Point.java
@@ -1,13 +1,17 @@
 package ro.unibuc.pao.homework;
 
 public class Point {
-
     private int x;
     private int y;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+    public Point(Point p)
+    {
+        this.x = p.x;
+        this.y = p.y;
     }
 
     public int getX() {
