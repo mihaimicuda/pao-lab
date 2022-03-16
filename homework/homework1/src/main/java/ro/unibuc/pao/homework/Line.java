ed03c8b67dda064a8d1aba1c563c14573a3d04fb
popmaria <popmaria461@yahoo.com>
2022-03-16 19:26:23 +0200

    tema

--- homework/homework1/src/main/java/ro/unibuc/pao/homework/Line.java
+++ homework/homework1/src/main/java/ro/unibuc/pao/homework/Line.java
@@ -15,5 +15,20 @@ public class Line {
 
     //TODO: add code here
 
+    public Point get_p1() {
+        return p1;
+    }
+
+    public Point get_p2() {
+        return p2;
+    }
+
+    public void set_p1(Point p1) {
+        this.p1 = p1;
+    }
+
+    public void set_p2(Point p2) {
+        this.p2 = p2;
+    }
 
 }
--- homework/homework1/src/main/java/ro/unibuc/pao/homework/ZigZagLine.java
+++ homework/homework1/src/main/java/ro/unibuc/pao/homework/ZigZagLine.java
@@ -1,25 +1,47 @@
 package ro.unibuc.pao.homework;
 
+import java.lang.Math;
 import java.util.List;
+import java.util.ArrayList;
 
 public class ZigZagLine extends Line {
 
     //TODO: add code here
+    ArrayList <Point> points1 = new ArrayList<Point>();
 
     public ZigZagLine(List<Point> points) {
         //TODO: add code here
+        for(Point p : points) {
+            points1.add(new Point(p.getX(), p.getY()));
+        }
     }
 
     public ZigZagLine(Point... points) {
         //TODO: add code here
+        for(Point p : points) {
+            points1.add(new Point(p.getX(), p.getY()));
+        }
     }
 
     public void addPoint(Point point) {
         //TODO: add code here
+        points1.add(new Point(point.getX(), point.getY()));
     }
 
     public int getLength() {
         //TODO: add code here
+        int length = 0;
+        Point last = points1.get(0);
+
+        if(points1.size() > 1) {
+
+            for(Point x : points1) {
+                double distance = Math.sqrt((x.getX()-last.getX())*(x.getX()-last.getX()) + (x.getY()-last.getY())*(x.getY()-last.getY()));
+                last = x;
+                length += distance;
+            }
+            return length;
+        }
         return 0;
     }
 
