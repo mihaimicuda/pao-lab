265b4392f08d3dff3a7133188a72c178b914305e
crisdijma <cristina.dijmarescu@s.unibuc.ro>
2022-04-11 13:18:13 +0300

    Tema finala cu testele trecute

--- homework/homework3/src/main/java/ro/unibuc/WordCounting.java
+++ homework/homework3/src/main/java/ro/unibuc/WordCounting.java
@@ -1,10 +1,25 @@
 package ro.unibuc;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class WordCounting {
     public static void main(String[] args) {
         FileWordCounter fileWordCounter = new FileWordCounter();
+
         //TODO: add code here to read from console and get the length limit
 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Introduceti numarul N: ");
+        int n = in.nextInt(); //citesc numarul n de la tastatura
+
+        //apelez functia getSizeN din clasa FileWordCounter pentru fisierul poezie 1 si numarul n dat de la tastaura
+        //pentru a rula exercitiul este posibil sa fie nevoie sa actualizati pathul fisierului
+        List<String> cuvinte = fileWordCounter.getSizeMoreThanN("homework/homework3/src/test/resources/poezie1.txt", n);
+
+        System.out.println(cuvinte);
 
     }
 }
