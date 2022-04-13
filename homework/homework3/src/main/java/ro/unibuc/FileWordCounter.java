50b23c2294e5af661a28e55d57d884557606ee70
BanesaruDenisa <77294618+BanesaruDenisa@users.noreply.github.com>
2022-04-13 13:54:57 +0300

    1

--- homework/homework3/src/main/java/ro/unibuc/FileWordCounter.java
+++ homework/homework3/src/main/java/ro/unibuc/FileWordCounter.java
@@ -1,7 +1,12 @@
 package ro.unibuc;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 /**
     TODO:
@@ -10,23 +15,65 @@ import java.util.List;
 
     In plus fata de lungime, care trebuie introdusa de la tastatura, putem filtra si dupa alte conditii(vedeti testele)
  */
+
 public class FileWordCounter {
 
-    //HINT: you could make one generic method to reuse for all three methods above
-    //HINT2: think about functional interfaces, one that verifies things in particular
+    public List<String> countN(String fileName, int n) throws FileNotFoundException {
 
-    public List<String> getSizeOne(String fileName) {
-        //TODO: add code here
-        return Collections.emptyList();
+     List<String> list = new ArrayList<String>();
+     try {
+         Scanner file = new Scanner(new File(fileName));
+         while (file.hasNextLine()) {
+             String line = file.nextLine();
+             String words[] = line.split(" ");
+
+             for (String w : words) {
+                 if (w.length() < n)
+                     list.add(w);
+             }
+         }
+         file.close();
+     } catch(FileNotFoundException e){
+         System.out.println("Error");
+     }
+        return list;
     }
 
-    public List<String> getSizeLessThan3(String fileName) {
-        //TODO: add code here
-        return Collections.emptyList();
+
+    public List<String> wordAppearances (String fileName, String word) {
+        List<String> list = new ArrayList<String>();
+
+        try {
+            Scanner file = new Scanner(new File(fileName));
+            while (file.hasNextLine()) {
+                String line = file.nextLine();
+                String words[] = line.split(" ");
+                for (String s : words) {
+                    if(s.replace("!", "").equals(word)){
+                        list.add(s);
+                    }
+                }
+            }
+            file.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error");
+        }
+        return list;
+    }
+
+
+    public List<String> getSizeOne(String fileName) throws FileNotFoundException {
+
+        return this.countN(fileName, 2);
+    }
+
+    public List<String> getSizeLessThan3(String fileName) throws FileNotFoundException {
+
+        return this.countN(fileName, 3);
     }
 
     public List<String> countWordAppearances(String fileName, String word) {
-        //TODO: add code here
-        return Collections.emptyList();
+
+        return this.wordAppearances(fileName, word);
     }
 }
