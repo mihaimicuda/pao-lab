1d84c3d9c91d058f6b8ef22f89b88366a5e4e48c
PetreVladMihaela <57861031+PetreVladMihaela@users.noreply.github.com>
2022-04-16 11:03:58 +0300

    Add files via upload

--- homework/homework3/src/main/java/ro/unibuc/FileWordCounter.java
+++ homework/homework3/src/main/java/ro/unibuc/FileWordCounter.java
@@ -5,8 +5,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
     TODO:
@@ -20,65 +23,57 @@ public class FileWordCounter {
     //HINT: you could make one generic method to reuse for all three methods above
     //HINT2: think about functional interfaces, one that verifies things in particular
 
-    public List<String> countWordsByLength(String fileName, int n) {
-        List<String> countedWords = new ArrayList<>();
-        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            reader.lines().forEach(line -> {
-                String[] words = line.split(" ");
-                for (String word : words) {
-                    if (word.length() > 1) {
-                        if (! Character.isLetter(word.charAt(0))) { //stringul nu incepe cu o litera
-                            word = word.substring(1); // elimina caracterul de la inceputul cuvantului
-                        }
-                        if (! Character.isLetter(word.charAt(word.length()-1))) { //stringul nu se termina cu o litera
-                            word = word.substring(0, word.length()-1); // elimina caracterul de la sfarsitul cuvantului
-                        }
-                    }
-                    if (word.length() > n) {
-                        countedWords.add(word);
-                    }
+    private Stream<String> getLineWords(String line) {
+        List<String> words = new ArrayList<>();
+        for (String word : line.split(" ")) {
+            if (word.length() == 1 && !Character.isLetter(word.charAt(0))) {
+                continue; // ignoram semnele de punctuatie
+            }
+            if (word.length() > 1) {
+                if (! Character.isLetter(word.charAt(0))) { //stringul nu incepe cu o litera
+                    word = word.substring(1); // elimina caracterul de la inceputul cuvantului
                 }
-            });
-        } catch (FileNotFoundException ex) {
+                if (! Character.isLetter(word.charAt(word.length()-1))) { //stringul nu se termina cu o litera
+                    word = word.substring(0, word.length()-1); // elimina caracterul de la sfarsitul cuvantului
+                }
+            }
+            words.add(word);
+        }
+        return words.stream();
+    }
+
+    public List<String> countWordsByLength(String fileName, int minLength) {
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            return reader.lines()
+                    .flatMap(this::getLineWords)
+                    .filter(word -> word.length() >= minLength)
+                    .collect(Collectors.toList());
+        } catch (FileNotFoundException e) {
             System.out.println("Fisierul nu exista!");
         }
-        catch(IOException ex) {
+        catch(IOException e) {
             System.out.println("Operatie de citire esuata!");
         }
-        return countedWords;
+        return Collections.emptyList();
     }
 
 
     public List<String> getFilteredWords(String fileName, Predicate<String> rightLength) {
-        List<String> allWords = countWordsByLength(fileName, 0);
-        List<String> countedWords = new ArrayList<>();
-        for (String word : allWords)
-            if (rightLength.test(word))
-                countedWords.add(word);
-        return countedWords;
+        return countWordsByLength(fileName, 1).stream()
+                .filter(rightLength)
+                .collect(Collectors.toList());
     }
-
 
     public List<String> getSizeOne(String fileName) {
-        Predicate<String> lengthOne = word -> word.length() == 1;
-        return getFilteredWords(fileName, lengthOne);
+        return getFilteredWords(fileName, word -> word.length() == 1);
     }
-
 
     public List<String> getSizeLessThan3(String fileName) {
-        Predicate<String> lengthLessThan3 = word -> word.length() < 3;
-        return getFilteredWords(fileName, lengthLessThan3);
+        return getFilteredWords(fileName, word -> word.length() < 3);
     }
-
 
     public List<String> countWordAppearances(String fileName, String word) {
-        Predicate<String> searchedLength = w -> w.length() == word.length();
-        List<String> potentialWords = getFilteredWords(fileName, searchedLength);
-        List<String> matchedWords = new ArrayList<>();
-        for (String w : potentialWords) {
-            if (word.equals(w))
-                matchedWords.add(w);
-        }
-        return matchedWords;
+        return getFilteredWords(fileName, word::equals);
     }
+
 }
