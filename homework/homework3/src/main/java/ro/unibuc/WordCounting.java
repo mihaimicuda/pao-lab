1f41ecb8e1013015e4547f933dbc456f2c3514de
Rares Popa <rares_teodor@yahoo.ro>
2022-04-19 21:37:46 +0300

    homework 3

--- homework/homework3/src/main/java/ro/unibuc/WordCounting.java
+++ homework/homework3/src/main/java/ro/unibuc/WordCounting.java
@@ -1,10 +1,14 @@
 package ro.unibuc;
 
+import java.util.Scanner;
+
 public class WordCounting {
     public static void main(String[] args) {
         FileWordCounter fileWordCounter = new FileWordCounter();
         //TODO: add code here to read from console and get the length limit
 
-
+        Scanner in = new Scanner(System.in);
+        int length = in.nextInt();
+        System.out.println(fileWordCounter.CountOfLength("src/test/resources/poezie1.txt", length));
     }
 }
