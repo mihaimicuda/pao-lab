40465b2e7b339f4a0a5e3c1df6eb37e830ef6bb1
MelaniaZotic <82153993+MelaniaZotic@users.noreply.github.com>
2022-04-19 22:31:54 +0300

    homework3

--- homework/homework3/src/main/java/ro/unibuc/WordCounting.java
+++ homework/homework3/src/main/java/ro/unibuc/WordCounting.java
@@ -1,10 +1,21 @@
 package ro.unibuc;
 
-public class WordCounting {
-    public static void main(String[] args) {
-        FileWordCounter fileWordCounter = new FileWordCounter();
-        //TODO: add code here to read from console and get the length limit
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
 
+public class WordCounting {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileWordCounter fileWordCounter = new FileWordCounter();
+
+        System.out.println("Input the n: ");
+        Scanner nr = new Scanner(System.in);
+        int n = nr.nextInt();
+
+        Scanner in = new Scanner("src/test/resources/poezie1.txt");
+        List<String> result = fileWordCounter.countN(String.valueOf(in), n);
+
+        System.out.println(result.size());
 
     }
 }
