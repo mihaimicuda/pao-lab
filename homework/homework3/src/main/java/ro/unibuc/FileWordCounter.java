e5c7da9f1ecb9069a62b90a761484b9e97f697a2
andib10 <81637678+andib10@users.noreply.github.com>
2022-04-20 03:26:35 +0300

    Homework 3

--- homework/homework3/src/main/java/ro/unibuc/FileWordCounter.java
+++ homework/homework3/src/main/java/ro/unibuc/FileWordCounter.java
@@ -1,7 +1,11 @@
 package ro.unibuc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 /**
     TODO:
@@ -12,21 +16,57 @@ import java.util.List;
  */
 public class FileWordCounter {
 
-    //HINT: you could make one generic method to reuse for all three methods above
-    //HINT2: think about functional interfaces, one that verifies things in particular
+    public List<String> sizeLessThanN(String fileName, int n) {
+        List<String> rezList = new ArrayList<String>();
+        try {
+            File file = new File(fileName);
+            Scanner fileReader = new Scanner(file);
+            while(fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                String[] words = line.split(" ");
+                for(String word : words) {
+                    if(word.length() < n) {
+                        rezList.add(word);
+                    }
+                }
+            }
+            fileReader.close();
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        return rezList;
+    }
+
+    public List<String> countAppearances(String fileName, String word) {
+        List<String> countList = new ArrayList<String>();
+        try {
+            File file = new File(fileName);
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                String[] words = line.split(" ");
+                for (String i : words) {
+                    if (i.equals(word) | (word+"!").equals(i)) {
+                        countList.add(i);
+                    }
+                }
+            }
+            fileReader.close();
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        return countList;
+    }
 
     public List<String> getSizeOne(String fileName) {
-        //TODO: add code here
-        return Collections.emptyList();
+        return this.sizeLessThanN(fileName, 2);
     }
 
     public List<String> getSizeLessThan3(String fileName) {
-        //TODO: add code here
-        return Collections.emptyList();
+        return this.sizeLessThanN(fileName, 3);
     }
 
     public List<String> countWordAppearances(String fileName, String word) {
-        //TODO: add code here
-        return Collections.emptyList();
+        return this.countAppearances(fileName, word);
     }
 }
--- homework/homework3/src/main/java/ro/unibuc/WordCounting.java
+++ homework/homework3/src/main/java/ro/unibuc/WordCounting.java
@@ -1,9 +1,16 @@
 package ro.unibuc;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class WordCounting {
     public static void main(String[] args) {
         FileWordCounter fileWordCounter = new FileWordCounter();
-        //TODO: add code here to read from console and get the length limit
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String fileName = "src/test/resources/poezie1.txt";
+        List<String> rez = fileWordCounter.sizeLessThanN(fileName, n);
+        System.out.println(rez.size());
 
 
     }
