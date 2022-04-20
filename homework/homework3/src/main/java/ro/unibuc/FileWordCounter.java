0ce081e2a29e0832dd53724ce2448a736daeedc3
OvidiuIordache1 <ovidiu.iordache.2020@gmail.com>
2022-04-20 20:06:49 +0300

    Homework 3

--- homework/homework3/src/main/java/ro/unibuc/FileWordCounter.java
+++ homework/homework3/src/main/java/ro/unibuc/FileWordCounter.java
@@ -1,32 +1,57 @@
 package ro.unibuc;
 
-import java.util.Collections;
-import java.util.List;
+import java.io.FileReader;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-/**
-    TODO:
-    scrieti o clasa java care sa returneze toate cuvinetele de lungime > n dintr-un fisier text
-    Lungimea n trebuie introdusa de la tastatura.
-
-    In plus fata de lungime, care trebuie introdusa de la tastatura, putem filtra si dupa alte conditii(vedeti testele)
- */
 public class FileWordCounter {
 
-    //HINT: you could make one generic method to reuse for all three methods above
-    //HINT2: think about functional interfaces, one that verifies things in particular
+    public List<String> getWords(String fileName) {
+        List<String> list = new ArrayList<String>();
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            Scanner file = new Scanner(fileReader);
+
+            while (file.hasNextLine()) {
+                String line = file.nextLine();
+                String[] words = line.split(" ");
+                for (String i: words) {
+                    // am sters si ’ pentru a calcula corect, dar va afisa natures, wandrest, etc. in loc de nature’s, wand’rest
+                    i = i.replaceAll("[“:,;.?!”’—-]", "");
+                    if (!i.chars().allMatch(Character::isDigit))  // verif. daca e cuvant
+                        list.add(i);
+                }
+            }
+            return list;
+
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        return Collections.emptyList();
+    }
+
+    public List<String> findByCustomFilter(String fileName, Predicate<String> filter) {
+        return getWords(fileName).stream()
+                .filter(filter)
+                .collect(Collectors.toList());
+    }
+
+
+    public List<String> getWordsMoreThanN(String fileName, int n) {
+        return findByCustomFilter(fileName, word -> word.length() > n);
+    }
 
     public List<String> getSizeOne(String fileName) {
-        //TODO: add code here
-        return Collections.emptyList();
+
+        return findByCustomFilter(fileName, word -> word.length() == 1);
     }
 
     public List<String> getSizeLessThan3(String fileName) {
-        //TODO: add code here
-        return Collections.emptyList();
+        return findByCustomFilter(fileName, word -> word.length() < 3);
     }
 
     public List<String> countWordAppearances(String fileName, String word) {
-        //TODO: add code here
-        return Collections.emptyList();
+        return findByCustomFilter(fileName, w -> Objects.equals(w, word));
     }
 }
--- homework/homework3/src/main/java/ro/unibuc/WordCounting.java
+++ homework/homework3/src/main/java/ro/unibuc/WordCounting.java
@@ -1,10 +1,17 @@
 package ro.unibuc;
 
+//import com.sun.tools.jdeprscan.scan.Scan;
+
+import java.util.Scanner;
+
 public class WordCounting {
     public static void main(String[] args) {
         FileWordCounter fileWordCounter = new FileWordCounter();
-        //TODO: add code here to read from console and get the length limit
+        System.out.println("n = ");
+        Scanner nr = new Scanner(System.in);
+        int n = nr.nextInt();
 
-
+        System.out.println(fileWordCounter.getWordsMoreThanN("src/test/resources/poezie1.txt", n));
+        System.out.println(fileWordCounter.getWordsMoreThanN("src/test/resources/poezie2.txt", n));
     }
 }
