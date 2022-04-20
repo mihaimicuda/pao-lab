30ae2416f9aa86547f96a38bdb65c12c720f3222
rimihai2001 <rimihai2001@gmail.com>
2022-04-20 14:10:47 +0300

    HW 3 Done

--- homework/homework3/src/main/java/ro/unibuc/WordCounting.java
+++ homework/homework3/src/main/java/ro/unibuc/WordCounting.java
@@ -1,10 +1,26 @@
 package ro.unibuc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class WordCounting {
     public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
         FileWordCounter fileWordCounter = new FileWordCounter();
-        //TODO: add code here to read from console and get the length limit
 
+        Integer n = fileWordCounter.inputN();
 
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+        String text = scanner.next();
+
+        String[] tmpWords = text.split("[!?.,() ]");
+
+        words.addAll(Arrays.asList(tmpWords));
+
+        System.out.println(fileWordCounter.getSizeMoreThanN(words,n).size());
+
+        scanner.close();
     }
 }
