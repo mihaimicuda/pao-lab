0e9a3a7c43916d25cf803ed7209cf0965a45c04a
MariaPop24 <popmaria461@yahoo.com>
2022-04-21 11:10:17 +0300

    Tema 3

--- homework/homework3/src/main/java/ro/unibuc/FileWordCounter.java
+++ homework/homework3/src/main/java/ro/unibuc/FileWordCounter.java
@@ -1,7 +1,11 @@
 package ro.unibuc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 /**
     TODO:
@@ -14,19 +18,68 @@ public class FileWordCounter {
 
     //HINT: you could make one generic method to reuse for all three methods above
     //HINT2: think about functional interfaces, one that verifies things in particular
+    public List<String> citire(String filename) {
+
+        try {
+            File fisier = new File(filename);
+            Scanner x = new Scanner(fisier);
+            List <String> cuvinte = new ArrayList<>();
+
+            while(fisier.hasNextLine()) {
+                String cuvant = fisier.next().split("[!?.,()-{}]")[0];
+                cuvinte.add(cuvant);
+            }
+
+            x.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+        }
+
+        return cuvinte;
+    }
 
     public List<String> getSizeOne(String fileName) {
         //TODO: add code here
-        return Collections.emptyList();
+
+        List <String> cuvinte = citire(fileName);
+        List <String> cuvinte_lungime1 = new ArrayList();
+
+        for(int i = 0; i < cuvinte.size(); i++) {
+            for(int j = 0; j < cuvinte[i].size(); j++)
+                if(cuvinte[i][j] == 1) {
+                    cuvinte3.add(cuvinte[i][j]);
+                }
+        }
+
+        return cuvinte_lungime1;
+
     }
 
     public List<String> getSizeLessThan3(String fileName) {
-        //TODO: add code here
-        return Collections.emptyList();
+
+        List <String> cuvinte = citire(fileName);
+        List <String> cuvinte3 = new ArrayList();
+
+        for(int i = 0; i < cuvinte.size(); i++) {
+            for(int j = 0; j < cuvinte[i].size(); j++)
+                if(cuvinte[i][j] < 3) {
+                    cuvinte3.add(cuvinte[i][j]);
+                }
+        }
+
+        return cuvinte3;
     }
 
     public List<String> countWordAppearances(String fileName, String word) {
         //TODO: add code here
-        return Collections.emptyList();
+        List <String> cuvinte = citire(fileName);
+
+        for(int i = 0; i < cuvinte.size(); i++) {
+            if(cuvinte[i].matches(word)) {
+                cuvinte.add(word);
+            }
+        }
+
+        return cuvinte;
     }
 }
